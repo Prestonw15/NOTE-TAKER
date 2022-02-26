43b0345ba9f2576dc76c2eeb84c5c06f7f2992d8
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-25 20:33:37 -0700

    added the function that'll allow me to create new notes later on

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,23 @@ const { notes } = require('./Develop/db/db.json');
 
 const PORT = process.env.PORT || 3003;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join('public')));
+
+// get notes from the db.json file
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+function createNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note)
+
+    fs.writeFileSync(
+        path.join(__dirname, '.Develop/db/db.json'),
+        JSON.stringify({notes: notesArray }, null, 2)
+    );
+    return body;
+};
