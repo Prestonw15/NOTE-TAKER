b73a72fe344a5feeae20e005638eedf89a880c64
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-25 22:38:51 -0700

    not sure hoping this fixes it

--- server.js
+++ server.js
@@ -6,63 +6,57 @@ const { notes } = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join('public')));
 
-// get notes from the db.json file 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 })
 
-function createNote(body, notesArray) {
-    
+function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note)
-
+    
     fs.writeFileSync(
         path.join(__dirname, './Develop/db/db.json'),
-        JSON.stringify({notes: notesArray }, null, 2)
+        JSON.stringify({notes: notesArray }, null, 1)
     );
 
     return body;
+
 };
 
 function validateNote(note) {
     if (!note.title || typeof note.title !== 'string') {
-      return false;
+        return false;
     }
     if (!note.text || typeof note.text !== 'string') {
-      return false;
+        return false;
     }
-   
     return true;
-  }
+
+}
 
 app.post('/api/notes', (req, res) => {
     console.log(req.body);
     req.body.id = notes.length.toString();
-    
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted. ');
-    } else {
-        const note = createNote(req.body, notes)
 
+    if (!validateNote(req.body)) {
+        res.status(400).send('Your note is not formatted correctly, please fix.');
+    } else {
+        const note = createNewNote(req.body, notes)
         res.json(note);
     }
-    
 })
 
-
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res,sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`Server now on port ${PORT}!`);
 });
